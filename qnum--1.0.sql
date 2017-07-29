d9977d4f31ff51b00d9c33e3234919ecfbed346c
xsaiter <xsaiter@mail.ru>
2017-07-29 07:55:59 +0300

    gcd

--- qnum--1.0.sql
+++ qnum--1.0.sql
@@ -4,3 +4,11 @@
 create function q_num_is_prime (int8) returns boolean
 as '$libdir/libqnum'
 language C immutable strict;
+
+create function q_num_gcd (int8, int8) returns int8
+as '$libdir/libqnum'
+language C immutable strict;
+
+create function q_num_is_relatively_prime (int8, int8) returns boolean
+as '$libdir/libqnum'
+language C immutable strict;
--- qnum.c
+++ qnum.c
@@ -16,10 +16,10 @@ PG_MODULE_MAGIC;
 
 int is_prime(long a)
 {
-    if(a < 2){
+    if (a < 2) {
         return 0;
     }
-    
+
     for (long i = 2; i * i <= a; ++i) {
         if (a % i == 0) {
             return 0;
@@ -28,12 +28,43 @@ int is_prime(long a)
     return 1;
 }
 
+long gcd(long a, long b) {
+  while (b != 0) {
+    int c = a % b;
+    a = b;
+    b = c;
+  }
+  return (a > 0) ? a : -a;
+}
+
+int is_relatively_prime(long a, long b) { return gcd(a, b) == 1; }
+
 PG_FUNCTION_INFO_V1(q_num_is_prime);
+PG_FUNCTION_INFO_V1(q_num_gcd);
+PG_FUNCTION_INFO_V1(q_num_is_relatively_prime);
 
 Datum
 q_num_is_prime(PG_FUNCTION_ARGS)
-{    
+{
     long n = PG_GETARG_INT64(0);
     int res = is_prime(n);
+    PG_RETURN_BOOL(res);
+}
+
+Datum
+q_num_gcd(PG_FUNCTION_ARGS)
+{
+    long a = PG_GETARG_INT64(0);
+    long b = PG_GETARG_INT64(1);
+    int res = gcd(a, b);
+    PG_RETURN_INT64(res);
+}
+
+Datum
+q_num_is_relatively_prime(PG_FUNCTION_ARGS)
+{
+    long a = PG_GETARG_INT64(0);
+    long b = PG_GETARG_INT64(1);
+    int res = is_relatively_prime(a, b);
     PG_RETURN_BOOL(res);
 }
